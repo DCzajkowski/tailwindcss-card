b8c3bdc2f86b02d4188f5e65582483fe838539c4
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2019-09-08 02:02:20 +0900

    Added production build ability

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  plugins: [require('tailwindcss')],
-};
+const plugins =
+  process.env.NODE_ENV === 'production'
+    ? [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss')({
+          content: ['./public/**/*.html'],
+          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+        require('cssnano'),
+      ]
+    : [require('tailwindcss')];
+
+module.exports = { plugins };
